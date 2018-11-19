bfa5840367f5972067faf697ae90a1355a4c220c
Radivarig <reslav.hollos@gmail.com>
2018-11-20 00:38:27 +0100

    Update year

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 #    <Uv Squares, Blender addon for reshaping UV vertices to grid.>
-#    Copyright (C) <2014> <Reslav Hollos>
+#    Copyright (C) <2018> <Reslav Hollos>
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
