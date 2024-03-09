507b951299e724e7922451d8b86d9959f9b46b01
Radivarig <reslav.hollos@gmail.com>
2024-03-09 05:17:27 +0100

    Version 1.15.0

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ bl_info = {
     "name": "UV Squares",
     "description": "UV Editor tool for reshaping quad selection to grid.",
     "author": "Reslav Hollos",
-    "version": (1, 14, 2),
+    "version": (1, 15, 0),
     "blender": (2, 80, 0),
     "location": "UV Editor > N Panel > UV Squares",
     "category": "UV",
